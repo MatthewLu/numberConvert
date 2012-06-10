4ad2c4d2297d8a32c1555a16ae83552f6e7fc4d8
Matthew Lu <matthewlu@192-168-1-8.tpgi.com.au>
2012-06-10 21:06:57 +1000

    Initial Commit

---
+++ numberConvert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  numberConvert
+//
+//  Created by Matthew Lu on 10/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
